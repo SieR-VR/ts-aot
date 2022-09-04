76ee006d8729826315648d5db9d3f4acc9b7623f
SieR-VR <nwh040512@gmail.com>
2022-09-04 21:08:01 +0900

    initial commit

---
+++ codegen.ts
@@ -0,0 +1,235 @@
+import { Enum } from 'ts-features';
+
+export namespace CPlusPlus {
+    export type SourceFile = (Enum<Macros> | Enum<Statement>)[];
+
+    export interface Macros {
+        Include: {
+            isBrace?: boolean,
+            target: string,
+        },
+        Simple: {
+            name: string,
+            value: string,
+        },
+        Function: {
+            name: string,
+            args: string[],
+            value: string,
+        }
+    }
+
+    export interface Statement {
+        Expression: { expr: Enum<Expression> },
+        Compound: { stmts: Enum<Statement>[] },
+        If: {
+            constexpr?: boolean, 
+            init_stmt?: Enum<Statement>, 
+            condition: Enum<Expression>,
+            if_stmt: Enum<Statement>,
+            else_stmt?: Enum<Statement>
+        },
+        While: {
+            condition: Enum<Expression>,
+            stmt: Enum<Statement>
+        },
+        DoWhile: {
+            stmt: Enum<Statement>
+            expr: Enum<Expression>,
+        },
+        For: {
+            init_stmt?: Enum<Statement>,
+            cond_expr: Enum<Expression>,
+            stmt: Enum<Statement>,
+        },
+        Break: [],
+        Continue: [],
+        Return: { expr?: Enum<Expression> },
+        Throw: { expr: Enum<Expression> },
+        FunctionDecl: { 
+            decl_specifier_seq: DeclSpecifierSeq,
+            declarator: {
+                noptr_declarator: NoPtrDeclarator,
+                param_list: Param[],
+            },
+            body: Enum<Statement>["Compound"]
+                | "delete"
+                | "default"
+        },
+        TemplateDecl: {
+            param_list: Enum<TemplateParam>[],
+            decl: Enum<Statement>["ClassDecl"]
+        },
+        NamespaceDecl: {
+            ns_name: string,
+            decls: Enum<Statement>["TemplateDecl"] 
+                 | Enum<Statement>["FunctionDecl"]
+                 | Enum<Statement>["NamespaceDecl"],
+        },
+        ClassDecl: {
+            key: "class" | "struct" | "union",
+            name: string,
+            base_clause: string,
+            member_specification: Enum<Statement>["SimpleDecl"]
+                                | Enum<Statement>["FunctionDecl"]
+                                | Enum<Statement>["TemplateDecl"]
+                                | Enum<Statement>["EnumDecl"]
+                                | Enum<Statement>["TypedefDecl"]
+        },
+        EnumDecl: {
+            key: "enum" | "enum class" | "enum struct",
+            name: string,
+            base_clause: string,
+            enumerator_list: { identifier: string, constexpr: string }[]
+        },
+        TypedefDecl: {
+            type: Enum<Type>,
+        },
+        SimpleDecl: {
+            decl_specifier_seq: DeclSpecifierSeq,
+            init_declarator_list: InitDeclator[],
+        },
+        TryBlock: {
+            try_stmt: Enum<Statement>["Compound"],
+            catch_result: Param,
+            catch_stmt: Enum<Statement>["Compound"],
+        },
+    }
+
+    export interface Type {
+        class: ClassSpecifier,
+        enum: EnumSpecifier,
+        simple: SimpleTypeSpecifier,
+        qualified: QualifiedTypeSpecifier, 
+        pointer: PointerTypeSpecifier,
+    }
+
+    export interface Expression {
+        BinaryOperator: {
+            operator: string,
+            left: Enum<Expression>,
+            right: Enum<Expression>,
+        },
+        AssignmentOperator: {
+            operator: string,
+            left: Enum<Expression>,
+            right: Enum<Expression>,
+        },
+        PrefixUnaryOperator: {
+            operator: string,
+            target: Enum<Expression>,
+        },
+        PostfixUnaryOperator: {
+            operator: string,
+            target: Enum<Expression>,
+        },
+        TernaryOperator: {
+            condition: Enum<Expression>,
+            left: Enum<Expression>,
+            right: Enum<Expression>, 
+        },
+        BinaryMemberAccessOperator: {
+            operator: "->" | "." | "[]" | "->*" | ".*",
+            left: Enum<Expression>,
+            right: Enum<Expression>,
+        },
+        UnaryMemberAccessOperator: {
+            operator: "*" | "&",
+            target: Enum<Expression>,
+        },
+        CastOperator: {
+            operator: "static_cast" | "dynamic_cast" | "const_cast" | "reinterpret_cast",
+            target: Enum<Expression>,
+            tparam: Enum<Type>
+        },
+        NewOperator: {
+            tparam: Enum<Type>,
+            initializer?: string,
+            count?: string,
+        },
+        DeleteOperator: {
+            target: Enum<Expression>,
+        },
+        SizeofOperator: {
+            target: Enum<Expression>,
+        },
+        SizeofPackOperator: {
+            target: string,
+        },
+        This: [],
+        Literal: number | string | { char: string } | boolean,
+        Lambda: {
+            captures: "&" | "=" | string[];
+            params: Param[],
+            body: Enum<Statement>["Compound"]
+        },
+        Identifier: {
+            name: string,
+        }
+    }
+
+    export interface ClassSpecifier {
+        typename: string;
+    }
+
+    export interface EnumSpecifier {
+        typename: string;
+    }
+
+    export type SimpleTypeSpecifier = {
+        typename: "char" | "wchar_t" | "bool" | "short" | "int" | "long" | "signed" | "unsigned" | "float" | "double" | "void";
+    }
+
+    export type QualifiedTypeSpecifier = {
+        typename: string;
+    }
+
+    export interface PointerTypeSpecifier {
+        typename: string
+    }
+
+    export interface DeclSpecifierSeq {
+        typedef?: boolean,
+        func_specifier?: "inline" | "virtual" | "explicit",
+        friend?: boolean,
+        storage_class_specifier?: "static" | "extern" | "mutable",
+        type_specifier: Enum<Type>
+    }
+
+    export type NoPtrDeclarator = string;
+
+    export interface NonTypeTemplateParamType {
+        integral: SimpleTypeSpecifier,
+        pointer: PointerTypeSpecifier,
+        enumeration: EnumSpecifier,
+    }
+
+    export interface NonTypeTemplateParam {
+        type: Enum<NonTypeTemplateParamType>,
+        name?: string,
+        default?: string,
+    }
+
+    export interface TypeTemplateParam {
+        name?: string,
+        default?: Enum<Type>,
+        param_pack?: boolean,
+    }
+
+    export interface Param {
+        decl_specifier_seq: DeclSpecifierSeq,
+        declarator?: string,
+        initializer?: string,
+    }
+
+    export interface InitDeclator {
+        declarator: string,
+        initalizer?: string,
+    }
+
+    export interface TemplateParam {
+        non_type_template_param: NonTypeTemplateParam,
+        type_template_param: TypeTemplateParam,
+        template_param: TemplateParam,
+    }
+}
---
+++ index.ts
@@ -0,0 +1,190 @@
+import ts from "typescript";
+import { match_number, Enum, to_enum } from "ts-features";
+import { CPlusPlus } from "./codegen";
+import { readFileSync } from "fs";
+
+type TypescriptStmt =
+    ts.SyntaxKind.ForStatement
+    | ts.SyntaxKind.DoStatement
+    | ts.SyntaxKind.IfStatement
+    | ts.SyntaxKind.TryStatement
+    | ts.SyntaxKind.ThrowStatement
+    | ts.SyntaxKind.BreakStatement
+    | ts.SyntaxKind.WhileStatement
+    | ts.SyntaxKind.ReturnStatement
+    | ts.SyntaxKind.ContinueStatement
+    | ts.SyntaxKind.VariableStatement
+    | ts.SyntaxKind.ExpressionStatement;
+
+
+const CppStatement = to_enum<CPlusPlus.Statement>();
+const CppExpression = to_enum<CPlusPlus.Expression>();
+const CppType = to_enum<CPlusPlus.Type>();
+
+type some = ts.ForInitializer
+
+function transpile(sourceFile: ts.SourceFile): CPlusPlus.SourceFile {
+    const visitExpr: (node: ts.Expression) => Enum<CPlusPlus.Expression> = (node: ts.Expression) => {
+        return {} as Enum<CPlusPlus.Expression>
+    }
+
+    const visitVariableDecl = (decl: ts.VariableDeclaration) => {
+        return CppStatement.SimpleDecl({
+            decl_specifier_seq: {
+                type_specifier: CppType.qualified({
+                    typename: decl.type!.getText(),
+                })
+            },
+            init_declarator_list: [
+                {
+                    declarator: decl.name.getText(),
+                    initalizer: decl.initializer?.getText(),
+                }
+            ]
+        });
+    }
+
+    const visitVariableDeclList = (decls: ts.VariableDeclarationList) => {
+        return CppStatement.SimpleDecl({
+            decl_specifier_seq: {
+                type_specifier: CppType.qualified({
+                    typename: decls.declarations[0].type!.getText(),
+                }),
+            },
+            init_declarator_list: decls.declarations.map((decl) => {
+                return {
+                    declarator: decl.name.getText(),
+                    initalizer: decl.initializer?.getText(),
+                };
+            })
+        });
+    }
+
+    const visitBlock: (node: ts.Block) => Enum<CPlusPlus.Statement> = (node: ts.Block) => {
+        return CppStatement.Compound({
+            stmts: node.statements.map(visitStmt),
+        });
+    }
+
+    const visitStmt: (node: ts.Statement) => Enum<CPlusPlus.Statement> = (node: ts.Statement) => {
+        return match_number<Enum<CPlusPlus.Statement>, TypescriptStmt>(node.kind as TypescriptStmt, {
+            [ts.SyntaxKind.ForStatement]: () => {
+                if (!ts.isForStatement(node))
+                    throw new Error("Something went wrong");
+
+                let init_stmt: Enum<CPlusPlus.Statement> | undefined;
+
+                if (node.initializer && ts.isVariableDeclarationList(node.initializer))
+                    init_stmt = visitVariableDeclList(node.initializer);
+                else 
+                    init_stmt = node.initializer && CppStatement.Expression({ expr: visitExpr(node.initializer) })
+
+                return CppStatement.For({
+                    init_stmt,
+                    cond_expr: visitExpr(node.condition!),
+                    stmt: visitStmt(node.statement),
+                });
+            },
+            [ts.SyntaxKind.DoStatement]: () => {
+                if (!ts.isDoStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.DoWhile({
+                    expr: visitExpr(node.expression),
+                    stmt: visitStmt(node.statement),
+                });
+            },
+            [ts.SyntaxKind.IfStatement]: () => {
+                if (!ts.isIfStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.If({
+                    condition: visitExpr(node.expression),
+                    if_stmt: visitStmt(node.thenStatement),
+                    else_stmt: node.elseStatement && visitStmt(node.elseStatement),
+                });
+            },
+            [ts.SyntaxKind.TryStatement]: () => {
+                if (!ts.isTryStatement(node))
+                    throw new Error("Something went wrong");
+
+                const variableDecl = visitVariableDecl(node.catchClause!.variableDeclaration!);
+
+                return CppStatement.TryBlock({
+                    try_stmt: visitStmt(node.tryBlock) as unknown as Enum<CPlusPlus.Statement>["Compound"],
+                    catch_result: {
+                        decl_specifier_seq: variableDecl.SimpleDecl.decl_specifier_seq,
+                        ...variableDecl.SimpleDecl.init_declarator_list[0]
+                    },
+                    catch_stmt: visitBlock(node.catchClause!.block).Compound,
+                })
+            },
+            [ts.SyntaxKind.ThrowStatement]: () => {
+                if (!ts.isThrowStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.Throw({
+                    expr: visitExpr(node.expression),
+                });
+            },
+            [ts.SyntaxKind.BreakStatement]: () => {
+                if (!ts.isBreakStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.Break([]);
+            },
+            [ts.SyntaxKind.WhileStatement]: () => {
+                if (!ts.isWhileStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.While({
+                    condition: visitExpr(node.expression),
+                    stmt: visitStmt(node.statement),
+                });
+            },
+            [ts.SyntaxKind.ReturnStatement]: () => {
+                if (!ts.isReturnStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.Return({
+                    expr: node.expression && visitExpr(node.expression),
+                });
+            },
+            [ts.SyntaxKind.ContinueStatement]: () => {
+                if (!ts.isContinueStatement(node))
+                    throw new Error("Something went wrong");
+                
+                return CppStatement.Continue([]);
+            },
+            [ts.SyntaxKind.VariableStatement]: () => {
+                if (!ts.isVariableStatement(node))
+                    throw new Error("Something went wrong");
+                
+                return visitVariableDeclList(node.declarationList);
+            },
+            [ts.SyntaxKind.ExpressionStatement]: () => {
+                if (!ts.isExpressionStatement(node))
+                    throw new Error("Something went wrong");
+
+                return CppStatement.Expression({
+                    expr: visitExpr(node.expression),
+                });
+            },
+        })
+    }
+
+    return sourceFile.statements.map(visitStmt);
+}
+
+const fileNames = process.argv.slice(2);
+
+fileNames.forEach(fileName => {
+    const sourceFile = ts.createSourceFile(
+        fileName,
+        readFileSync(fileName).toString(),
+        ts.ScriptTarget.ES2015,
+        true,
+    )
+
+    console.log(transpile(sourceFile));
+})
